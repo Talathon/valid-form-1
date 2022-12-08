e6d0aab463be7e458fa0ad7a6b41f756a9a8a609
AhmadTalatkhon <a.talatkhon@gmail.com>
2022-12-08 23:58:04 +0500

    tamom

---
+++ 2-qisim/script.js
@@ -0,0 +1,19 @@
+function func(e){
+    let text = document.querySelector(".lorem").innerText;
+    let searchValue = e.target.value;
+
+
+    let natija = text.includes(searchValue);
+
+    let success = document.querySelector(".true");
+    let failed = document.querySelector(".false");
+
+    if(natija) {
+        success.classList.remove("d-none");
+        failed.classList.add("d-none")
+    } else {
+        success.classList.add("d-none")
+        failed.classList.remove("d-none")
+    }
+
+}
